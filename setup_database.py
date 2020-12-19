fab0920375ed77041ce195c6f60af91e870f9391
Grigory Nikitin <grigory.s.nikitin@gmail.com>
2020-12-19 18:08:54 +0300

    added all basic tables to setup_database

--- setup_database.py
+++ setup_database.py
@@ -30,14 +30,58 @@ def open_close_database(func):
 @open_close_database
 def setup_database(mydb, mycursor):
     '''function to create all needed tables if they don't exist'''
+
+    # table with command calls
     mycursor.execute('''
-    create table if not exists command_calling (
+    create table if not exists command_calls (
         update_id int,
-        message_datetime datetime,
+        call_dttm datetime,
         chat_id int,
-        username varchar(255),
+        user_id varchar(255),
         command_name varchar(255)
     );''')
+
+    # table with user info
+    # every user must appear only once
+    mycursor.execute('''
+    create table if not exists users (
+        user_id int,
+        username varchar(255),
+        language_code varchar(255),
+        start_dttm datetime
+    );
+    ''')
+
+    # table with packs that user can use
+    # by default everyone should have default pack
+    mycursor.execute('''
+    create table if not exists user_packs (
+        user_id int,
+        pack_id int,
+        added_dttm datetime
+    );
+    ''')
+
+    # table with sticker packs info
+    mycursor.execute('''
+    create table if not exists packs (
+        pack_id int,
+        pack_name varchar(255),
+        pack_author_id int,
+        create_dttm datetime
+    );
+    ''')
+
+    # table with stickers in pack
+    mycursor.execute('''
+    create table if not exists pack_stickers (
+        pack_id int,
+        sticker_id int,
+        sticker_shortcut varchar(255),
+        user_added_id int,
+        added_dttm datetime
+    );
+    ''')
 
 
 if __name__ == '__main__':
